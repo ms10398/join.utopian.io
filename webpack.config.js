319fb6606eca7de835a96d148596f900c4bdc101
mktcode <kontakt@markus-kottlaender.de>
2018-03-13 20:09:05 +0100

    configured babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
