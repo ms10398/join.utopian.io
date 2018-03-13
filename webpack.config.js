6abe7f88316fa8746d76645f7a4ae0da64dd59e5
mktcode <kontakt@markus-kottlaender.de>
2018-03-13 20:48:46 +0100

    minify html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -32,5 +33,14 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: '../index.html',
+      minify: {
+        collapseWhitespace: true
+      }
+    })
+  ]
 };
